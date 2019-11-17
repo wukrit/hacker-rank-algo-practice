959db33146ec22eec76cc8918daaf820e35f144b
Wukrit <sukritwalia@gmail.com>
2019-11-17 16:16:26 -0500

    completed sock-merchant

---
+++ sock-merchant.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    let count = 0
+    let stack = []
+    ar.forEach(item => {
+        if (stack.includes(item)) {
+            let i = stack.indexOf(item)
+            stack.splice(i, 1)
+            count ++
+        } else {
+            stack.push(item)
+        }
+    })
+    return count
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
