382a02d1df45b83d4d9a8333b3bb9285279078d2
Wukrit <sukritwalia@gmail.com>
2019-11-17 17:14:30 -0500

    completed jumping-on-the-clouds

---
+++ jumping-on-the-clouds.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+    let pos = 0
+    let jumps = 0
+    while (pos < (c.length - 1)) {
+        c[pos + 2] === 0 ? pos += 2 : pos ++
+        jumps ++
+    }
+    return jumps
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
